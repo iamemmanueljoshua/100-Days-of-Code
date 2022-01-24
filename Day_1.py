2c748ecc135b33ecebff523b721ecfa6241b72d1
Emmanuel Joshua <iamemmanueljoshua@gmail.com>
2022-01-24 14:55:44 -0600

    Update and rename Day 1: Project Band Name Generator.py to Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,13 @@
+*************************************************************************"""
+# Day 1: Project Band Name Generator.
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line, see the example at:
+*************************************************************************"""
+
+print("Welcome to the Band Name Generator")
+user_city = input("What city did you grew up in? \n")
+user_pet_name = input("What is the name of your pet? \n")
+print("The name of your Band is " + user_city + "" + user_pet_name)
