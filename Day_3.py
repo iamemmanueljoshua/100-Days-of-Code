04c0589aaa3f5499617ec2777a913d3a9dece243
Emmanuel Joshua <iamemmanueljoshua@gmail.com>
2022-01-25 15:28:37 -0600

    Update Day_3.py

--- Day_3.py
+++ Day_3.py
@@ -143,3 +143,76 @@ elif score >= 40 and score <= 50:
     print(f"Your score is {score}, you are alright together.")
 else:
     print(f"Your score is {score}.")
+
+    
+***********************************************************************************************************"""
+Treasure Island
+Make your own "Choose Your Own Adventure" game. Use conditionals such as if, else, and elif statements to lay out the logic and the story's path in your program.
+'You\'re at a crossroad. Where do you want to go? Type "left" or "right"'
+'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.'
+"You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?"
+"It's a room full of fire. Game Over."
+"You found the treasure! You Win!"
+"You enter a room of beasts. Game Over."
+"You chose a door that doesn't exist. Game Over."
+"You get attacked by an angry trout. Game Over."
+"You fell into a hole. Game Over."
+
+***********************************************************************************************************"""
+    
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+user_choice = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right" \n')
+
+if user_choice == "left":
+  option1 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n')
+  if option1 == "wait":
+    option2 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+    if option2 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif option2 == "yellow":
+      print("You found the treasure! You Win!")
+    elif option2 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  elif option1 == "swim":
+    print("You get attacked by an angry trout. Game Over.")
+elif user_choice == "right":
+  print("You fell into a hole. Game Over.")
+    
+    
+  
+  
+
+
+
+
+
+
+
+
